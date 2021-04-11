f5a1f68830f00640622991a4a38896f8b7b3d51e
Megs <Megs.close@gmail.com>
2021-04-11 16:15:14 -0700

    playing around with some functions

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const router = require('express').Router();
+
+// The following HTML routes should be created:
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+  });
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const notes = require("./db/db.json");
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+console.log(notes);
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+    next();
+};
+
+// init middleware
+app.use(logger);
+
+// gets all notes
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// sets static folder
+app.use(express.static('public'));
+
+// Use apiRoutes
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
 
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
